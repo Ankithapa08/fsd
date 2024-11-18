4dd3c942a9d1fa01770cbe9df93f4009397e4ccb
Ankithapa08 <lt17086@gmail.com>
2024-11-18 13:07:57 +0530

    push

---
+++ Day12/netflix-clone/src/movies.js
@@ -0,0 +1,37 @@
+const movies=[{
+    id:1,
+    image:"https://rukminim2.flixcart.com/image/612/612/av-media/movies/q/w/c/gangs-of-wasseypur-i-original-imadnzhqzxzrgumh.jpeg?q=70",
+    title:"Gangs of Wasseypur",
+    language:"Hindi",
+    cost:499,
+},
+{
+    id:2,
+    image:"https://rukminim2.flixcart.com/image/612/612/av-media/movies/m/k/b/jennifers-body-original-imad7hz2ts7sxgrf.jpeg?q=70",
+    title:"Jennifer's body",
+    language:"Hindi",
+    cost: 499,
+},
+{
+    id:3,
+    image:"https://rukminim2.flixcart.com/image/612/612/klb78nk0/movie/o/i/s/1999-dvd-captain-vedio-hindi-biwi-no-1-original-imagygmkhrhemsnx.jpeg?q=70",
+    title:"biwi no.1",
+    language:"hindi",
+    cost: 200,
+},
+{
+    id:4,
+    image:"https://rukminim2.flixcart.com/image/612/612/klb78nk0/movie/o/i/s/1999-dvd-captain-vedio-hindi-biwi-no-1-original-imagygmkhrhemsnx.jpeg?q=70",
+    title:"biwi no.1",
+    language:"hindi",
+    cost: 200,
+},
+{
+    id:5,
+    image:"https://rukminim2.flixcart.com/image/612/612/klb78nk0/movie/o/i/s/1999-dvd-captain-vedio-hindi-biwi-no-1-original-imagygmkhrhemsnx.jpeg?q=70",
+    title:"biwi no.1",
+    language:"hindi",
+    cost: 200,
+},
+]
+export default movies;
